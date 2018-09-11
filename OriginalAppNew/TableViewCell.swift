c5bfad8bebb065687ac8be459b1042aa4e23f446
市川龍星 <[02262ne1@gmail.com]>
2018-09-11 18:42:43 +0900

    初コミ

---
+++ OriginalAppNew/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  OriginalAppNew
+//
+//  Created by 市川龍星 on 2018/09/11.
+//  Copyright © 2018年 市川龍星. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ OriginalAppNew/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  OriginalAppNew
+//
+//  Created by 市川龍星 on 2018/09/11.
+//  Copyright © 2018年 市川龍星. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
